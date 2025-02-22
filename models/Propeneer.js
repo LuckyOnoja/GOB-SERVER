9476c5e336f5289a5c511be87d6e4473709e1135
Lucky LK <134052852+LuckyOnoja@users.noreply.github.com>
2025-02-22 04:43:24 -0800

    env work allow cors

--- models/Propeneer.js
+++ models/Propeneer.js
@@ -4,7 +4,7 @@ const PropeneerSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }], // Reference to transactions
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }], 
 });
 
 module.exports = mongoose.model("Propeneer", PropeneerSchema);
